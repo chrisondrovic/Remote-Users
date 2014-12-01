2767644789a7dbb57d4dadd59d0e25df4ed2488f
chris.ondrovic <chris.ondrovic@inin.com>
2014-11-30 20:15:44 -0500

    Initial

---
+++ Remote-Users/DXSplashScreen/Loading.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Remote_Users {
+
+    /// <summary>
+    /// Interaction logic for Loading.xaml
+    /// </summary>
+    public partial class Loading : UserControl {
+
+        public Loading() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Remote-Users/ServerList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Remote_Users {
+
+    public static class ServerList {
+
+        /// <summary>
+        /// Sets the servers.
+        /// </summary>
+        /// <param name="servers">The servers.</param>
+        internal static void SetServers(ObservableCollection<String> servers) {
+            Servers = servers;
+        }
+
+        /// <summary>
+        /// The servers
+        /// </summary>
+        public static ObservableCollection<String> Servers = new ObservableCollection<string>();
+    }
+}
